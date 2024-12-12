e169e66a28a21465b67141c42318aa83263eb5bf
stefanMadzharov <83451593+stefanMadzharov@users.noreply.github.com>
2024-12-12 15:36:26 +0000

    First try of day 12 Part 2

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,6 +1,8 @@
+#![feature(let_chains)]
 advent_of_code::solution!(12);
 use Direction::*;
 
+#[derive(PartialEq)]
 enum Direction {
     Up,
     Down,
@@ -38,14 +40,100 @@ impl Direction {
             }
         }
     }
+
+    fn go_left(&self, coordinates: (usize, usize)) -> Option<(usize, usize)> {
+        let (i, j) = coordinates;
+        match *self {
+            Up => Some((i, j.checked_sub(1)?)),
+            Down => Some((i, j + 1)),
+            Left => Some((i + 1, j)),
+            Right => Some((i.checked_sub(1)?, j)),
+        }
+    }
+
+    fn go_forward(&self, coordinates: (usize, usize)) -> Option<(usize, usize)> {
+        let (i, j) = coordinates;
+        match *self {
+            Up => Some((i.checked_sub(1)?, j)),
+            Down => Some((i + 1, j)),
+            Left => Some((i, j.checked_sub(1)?)),
+            Right => Some((i, j + 1)),
+        }
+    }
 }
 const DIRECTIONS: [Direction; 4] = [Up, Down, Right, Left];
+
+struct BulkCounter {
+    direction: Direction,
+    position: (usize, usize),
+    finished: bool,
+    started: bool,
+    consecutive_turns: u8,
+    edges: u32,
+}
+
+impl BulkCounter {
+    fn new(position: (usize, usize)) -> Self {
+        Self {
+            direction: Up,
+            position,
+            finished: false,
+            started: false,
+            consecutive_turns: 0,
+            edges: 0,
+        }
+    }
+
+    fn move_next_position(&mut self, coordinates: &Vec<(usize, usize)>) {
+        if self.consecutive_turns < 4 {
+            if self.started && self.position == coordinates[0] {
+                if self.direction == Left {
+                    self.edges += 1;
+                }
+                self.finished = true
+            } else {
+                if let Some(future_position) = self.direction.go_left(self.position)
+                    && coordinates.contains(&future_position)
+                {
+                    match self.direction {
+                        Up => self.direction = Left,
+                        Left => self.direction = Down,
+                        Down => self.direction = Right,
+                        Right => self.direction = Up,
+                    };
+                    self.edges += 1;
+                    self.position = future_position;
+                } else {
+                    if let Some(future_position) = self.direction.go_forward(self.position)
+                        && coordinates.contains(&future_position)
+                    {
+                        self.position = future_position;
+                        self.started = true;
+                        self.consecutive_turns = 0;
+                    } else {
+                        match self.direction {
+                            Up => self.direction = Right,
+                            Right => self.direction = Down,
+                            Down => self.direction = Left,
+                            Left => self.direction = Up,
+                        };
+                        self.consecutive_turns += 1;
+                        self.edges += 1;
+                    }
+                }
+            }
+        } else {
+            self.finished = true;
+        }
+    }
+}
 
 #[derive(Debug)]
 struct Region {
     symbol: char,
     area: u32,
     perimeter: u32,
+    cells: Vec<(usize, usize)>,
 }
 
 impl Region {
@@ -54,6 +142,7 @@ impl Region {
             symbol,
             area: 0,
             perimeter: 0,
+            cells: vec![],
         }
     }
 
@@ -66,6 +155,7 @@ impl Region {
         let (i, j) = coordinates;
         map_processed[i][j] = true;
         self.area += 1;
+        self.cells.push(coordinates);
         for direction in DIRECTIONS {
             if let Some((symbol, new_coordinates)) = direction.get(&map, coordinates) {
                 if symbol == self.symbol {
@@ -79,6 +169,25 @@ impl Region {
                 self.perimeter += 1;
             }
         }
+    }
+
+    fn get_bulk_perimeter(&mut self) -> u32 {
+        self.cells.sort_by(|a, b| {
+            if a.0.cmp(&b.0).is_eq() {
+                a.1.cmp(&b.1)
+            } else {
+                a.0.cmp(&b.0)
+            }
+        });
+        let mut bulk_counter = BulkCounter::new(self.cells[0]);
+        while !bulk_counter.finished {
+            bulk_counter.move_next_position(&self.cells);
+        }
+        bulk_counter.edges
+    }
+
+    fn contains(&mut self, other: &mut Region) -> bool {
+        todo!()
     }
 }
 
@@ -101,6 +210,7 @@ fn find_regions(map: Vec<Vec<char>>) -> Vec<Region> {
 }
 
 fn _print_map(map: &Vec<Vec<char>>) {
+    println!("{}", String::from("_").repeat(120));
     for row in map {
         for c in row {
             print!("{c}");
@@ -121,8 +231,22 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let map: Vec<Vec<char>> = input.lines().map(|row| row.chars().collect()).collect();
+    _print_map(&map);
+    let mut regions = find_regions(map);
+    for region in regions.iter_mut() {
+        println!(
+            "{region:?} with bulk perimeter: {}",
+            region.get_bulk_perimeter()
+        );
+    }
+    Some(
+        regions
+            .iter_mut()
+            .map(|region| region.area * region.get_bulk_perimeter())
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -150,6 +274,22 @@ mod tests {
         let result = part_two(&advent_of_code::template::read_file_part(
             "examples", DAY, 1,
         ));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(80));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(436));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 4,
+        ));
+        assert_eq!(result, Some(236));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 5,
+        ));
+        assert_eq!(result, Some(368));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 3,
+        ));
+        assert_eq!(result, Some(1206));
     }
 }
