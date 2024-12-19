5516f848d38d74da46056b0c79ae56f59212048e
stefanMadzharov <83451593+stefanMadzharov@users.noreply.github.com>
2024-12-19 14:27:44 +0000

    Day19 done

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -2,17 +2,95 @@ use std::collections::{hash_map::Entry, HashMap};
 
 advent_of_code::solution!(19);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let available_patterns = get_available_patterns(input);
-    println!("{available_patterns:?}");
-    None
+fn is_possible(design: &Vec<char>, available_patterns: &HashMap<char, Vec<Vec<char>>>) -> bool {
+    if design.len() == 0 {
+        true
+    } else {
+        let first_color = design[0];
+        available_patterns
+            .get(&first_color)
+            .unwrap_or(&vec![])
+            .iter()
+            .filter(|&pattern| {
+                pattern
+                    .iter()
+                    .zip(design)
+                    .map(|pair| *pair.0 == *pair.1)
+                    .all(|can_continue| can_continue)
+            })
+            .any(|pattern| {
+                pattern.len() <= design.len()
+                    && is_possible(&design[pattern.len()..].to_vec(), available_patterns)
+            })
+    }
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn count_possibilities(
+    design: &Vec<char>,
+    available_patterns: &HashMap<char, Vec<Vec<char>>>,
+    cache: &mut HashMap<Vec<char>, u64>,
+) -> u64 {
+    if design.len() == 0 {
+        1
+    } else {
+        match cache.get(design) {
+            Some(cached_value) => *cached_value,
+            None => {
+                let first_color = design[0];
+                let result = available_patterns
+                    .get(&first_color)
+                    .unwrap_or(&vec![])
+                    .iter()
+                    .filter(|&pattern| {
+                        pattern
+                            .iter()
+                            .zip(design)
+                            .map(|pair| *pair.0 == *pair.1)
+                            .all(|can_continue| can_continue)
+                    })
+                    .map(|pattern| {
+                        if pattern.len() <= design.len() {
+                            count_possibilities(
+                                &design[pattern.len()..].to_vec(),
+                                available_patterns,
+                                cache,
+                            )
+                        } else {
+                            0
+                        }
+                    })
+                    .sum();
+                cache.insert(design.clone(), result);
+                result
+            }
+        }
+    }
 }
 
-fn get_available_patterns(input: &str) -> HashMap<char, Vec<Vec<char>>> {
+pub fn part_one(input: &str) -> Option<u64> {
+    let available_patterns = parse_available_patterns(input);
+    let wanted_designs = parse_wanted_designs(input);
+    Some(
+        wanted_designs
+            .iter()
+            .filter(|&design| is_possible(design, &available_patterns))
+            .count() as u64,
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let available_patterns = parse_available_patterns(input);
+    let wanted_designs = parse_wanted_designs(input);
+    let mut cache: HashMap<Vec<char>, u64> = HashMap::new();
+    Some(
+        wanted_designs
+            .iter()
+            .map(|design| count_possibilities(design, &available_patterns, &mut cache))
+            .sum(),
+    )
+}
+
+fn parse_available_patterns(input: &str) -> HashMap<char, Vec<Vec<char>>> {
     let mut available_patterns: HashMap<char, Vec<Vec<char>>> = HashMap::new();
     let patterns: Vec<&str> = input
         .lines()
@@ -21,8 +99,8 @@ fn get_available_patterns(input: &str) -> HashMap<char, Vec<Vec<char>>> {
         .collect();
     for pattern in patterns {
         let pattern_as_chars = pattern.chars().collect::<Vec<char>>();
-        let first_letter = pattern_as_chars[0];
-        match available_patterns.entry(first_letter) {
+        let first_color = pattern_as_chars[0];
+        match available_patterns.entry(first_color) {
             Entry::Occupied(mut occupied_entry) => {
                 occupied_entry.get_mut().push(pattern_as_chars);
             }
@@ -32,6 +110,14 @@ fn get_available_patterns(input: &str) -> HashMap<char, Vec<Vec<char>>> {
         }
     }
     available_patterns
+}
+
+fn parse_wanted_designs(input: &str) -> Vec<Vec<char>> {
+    input
+        .lines()
+        .skip(2)
+        .map(|line| line.chars().collect())
+        .collect()
 }
 
 #[cfg(test)]
@@ -47,6 +133,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(16));
     }
 }
